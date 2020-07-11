44dca856ef8105fae8f806f60a744bc0da661679
Dinh Thai <dinh_k_thai@hotmail.com>
2020-07-10 22:29:29 -0600

    Add checkbox, complete button, delete button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-// Citation: line 3 to 78 are taken from James Grieve in class "js practice - Shopping List" example. Link: https://github.com/TECHCareers-by-Manpower/js-practice
-
+// Citation: line 3 to 16, 27 to 71 and 80 to 87 are taken from James Grieve in class "js practice - Shopping List" example. 
+// Link: https://github.com/TECHCareers-by-Manpower/js-practice
 {
     console.log("Script starting!");
     // querySelector will use CSS selector syntax to select HTML elements.
@@ -9,16 +9,25 @@
     // <a href="#">Add Item</a>
     const addButton = document.querySelector("a");
     // <ul class="empty">
-    const list = document.querySelector("ul");
+    const list = document.querySelector("section>ul");
     // <p id="error-output"></p>
     const error = document.querySelector("#error-output");
     // Running list of all our items.
-    let itemArray = [];
+    itemArray = [];
+
+    // Button delete to make a function to hide the current list item
+    close = document.getElementsByClassName("close");
+   
+    // Button complete function to bring date to completed to-do list.
+    const asidelist = document.querySelector("aside>ul");   
+    
+    // Button complete function to bring item to completed to-do list.
+    complete = document.getElementsByClassName("complete");
 
     addButton.addEventListener("click", () =>
         {
             // Keep track of if the current submission has an error.
-            let errors = false;
+            errors = false;
 
             // If the input is empty.
             if (input.value.trim() === "")
@@ -40,7 +49,7 @@
                 error.innerText = "";
 
                 // 1. Create a list item element.
-                let newListItem = document.createElement("li");
+                newListItem = document.createElement("li");
 
                 // 2. Add the text from the text field to the list item.
                 // P.S. Also removing trailing and leading whitespace.
@@ -59,20 +68,76 @@
                     // If the list still has the empty class, it should only
                     // have the "no items" li.
                     list.children[0].remove();
-
                 }
+
+                // Add checkbox to Active To-Dos item.
+                // Citation: w3schools.com
+                // Link: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_checkbox_create
+                checkbox = document.createElement("input");
+                checkbox.setAttribute("type", "checkbox");
+                newListItem.appendChild(checkbox);
+
                 // 3. Add the list item to the list.
-                list.appendChild(newListItem, id="demo" );
+                list.appendChild(newListItem);
                     
                 // 4. Clear the text input. 
                 input.value = "";
 
                 // Reset Focus To Input After Adding
                 input.focus();
+
+                // Input Date and Time.
+                // Citation: phonixnap.com
+                // Link: https://phoenixnap.com/kb/how-to-get-the-current-date-and-time-javascript
+                const today=new Date();
+                span2 = document.createElement("SPAN2");
+                txt2 = document.createTextNode((today.getMonth()+1)+'-'+today.getDate()+' '+today.getFullYear()+'-'+today.getHours()+ ':' +today.getMinutes()+ ':' +today.getSeconds());
+
+                span = document.createElement("SPAN");
+                txt = document.createTextNode("Complete");
+                // Button co // Add Date.
+                span.className = "complete";
+                
+                span1 = document.createElement("SPAN1");
+                txt1 = document.createTextNode("Delete");
+                // Button delete icon.
+                span1.className = "close";
+
+                // Date-time displayed.
+                span2.appendChild(txt2);
+                newListItem.appendChild(span2);
+
+                // Display button complete on active to-do list
+                span.appendChild(txt);
+                newListItem.appendChild(span);
+                //Display button delete on active to-do list
+                span1.appendChild(txt1);
+                newListItem.appendChild(span1);
+
+                // Button delete function.
+                for (i = 0; i < close.length; i++) {
+                    close[i].onclick = function () {
+                        div = this.parentElement;
+                        div.style.display = "none";
+                    }
+                }
+
+                // Button complete function.
+                for (i = 0; i < complete.length; i++) {
+                    complete[i].onclick = function () 
+                    {
+                        // Button complete function.
+                        sectionorigin = this.parentElement;  
+                        
+                        // Button complete function.
+                        asidelistItem = document.createElement("li");
+                            
+                        // Button complete press to move list to complete to-do list.
+                        asidelist.appendChild(sectionorigin);        
+                    }
+                }
             }
         }
     );
-    
-
     console.log("Script ending!");
 }
